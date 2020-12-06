0edd6af37b5e1dbafc02e63aa0b3e7a0c640efb7
khoivc <vucokhoilsls@gmail.com>
2020-12-07 00:43:42 +0700

    bulk update

--- bulkUpsert.js
+++ bulkUpsert.js
@@ -13,13 +13,50 @@ function bulkUpsert(allItemsArray, Model) {
   });
 }
 
+function bulkUpsertV2(allItemsArray, Model) {
+  let bulk = Model.collection.initializeOrderedBulkOp();
+
+  return new Promise((resolve, reject) => {
+    allItemsArray.forEach((item, index) => {
+      bulk.find(item.query).upsert().updateOne({ $set: item.document });
+      if (index % 1000 == 0)
+        bulk.execute(function (err, bulkers) {
+          bulk = Model.collection.initializeOrderedBulkOp();
+          if (err) {
+            return reject(err);
+          }
+        });
+    });
+    bulk.execute((err, bulkers) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(bulkers);
+    });
+  });
+}
+
 function updateMany(allItemsArray, Model) {
   return Promise.all(
     allItemsArray.map((item) =>
-      Model.findOneAndUpdate(item.query, item.document)
+      Model.findOneAndUpdate(item.query, item.document, { upsert: true })
     )
+  );
+}
+
+function bulkWriteUpdate(allItemsArray, Model) {
+  return Model.bulkWrite(
+    allItemsArray.map((item) => ({
+      updateOne: {
+        filter: item.query,
+        update: item.document,
+        upsert: true,
+      },
+    }))
   );
 }
 
 exports.updateMany = updateMany;
 exports.bulkUpsert = bulkUpsert;
+exports.bulkUpsertV2 = bulkUpsertV2;
+exports.bulkWriteUpdate = bulkWriteUpdate;
--- test-bulkUpsert.js
+++ test-bulkUpsert.js
@@ -1,4 +1,9 @@
-const { bulkUpsert, updateMany } = require("./bulkUpsert");
+const {
+  bulkUpsert,
+  updateMany,
+  bulkWriteUpdate,
+  bulkUpsertV2,
+} = require("./bulkUpsert");
 const bulkUpsertModel = require("./models/bulkUpsert");
 const connectMongo = require("./core/connectMongo");
 
@@ -17,10 +22,11 @@ async function main() {
   await connectMongo();
 
   console.time("time-create-data");
-  const data = createData(10000);
+  const data = createData(5000);
   console.timeEnd("time-create-data");
+  console.log("data length: ", data.length);
 
-  console.log("\n\nbulkUpdate");
+  console.log("\n\nbulkUpdate use initializeUnorderedBulkOp");
 
   await bulkUpsertModel.deleteMany();
 
@@ -32,7 +38,19 @@ async function main() {
   await bulkUpsert(data, bulkUpsertModel);
   console.timeEnd("time-update-data-1");
 
-  console.log("\n\nupdateMany");
+  console.log("\n\nbulkUpdateV2 use initializeUnorderedBulkOp");
+
+  await bulkUpsertModel.deleteMany();
+
+  console.time("time-insert-data-1-v2");
+  await bulkUpsertV2(data, bulkUpsertModel);
+  console.timeEnd("time-insert-data-1-v2");
+
+  console.time("time-update-data-1-v2");
+  await bulkUpsertV2(data, bulkUpsertModel);
+  console.timeEnd("time-update-data-1-v2");
+
+  console.log("\n\nupdateMany use findOneAndUpdate");
 
   await bulkUpsertModel.deleteMany();
 
@@ -43,6 +61,18 @@ async function main() {
   console.time("time-update-data-2");
   await updateMany(data, bulkUpsertModel);
   console.timeEnd("time-update-data-2");
+
+  console.log("\n\nbulkWriteUpdate");
+
+  await bulkUpsertModel.deleteMany();
+
+  console.time("time-insert-data-3");
+  await bulkWriteUpdate(data, bulkUpsertModel);
+  console.timeEnd("time-insert-data-3");
+
+  console.time("time-update-data-3");
+  await bulkWriteUpdate(data, bulkUpsertModel);
+  console.timeEnd("time-update-data-3");
 }
 
 main()
